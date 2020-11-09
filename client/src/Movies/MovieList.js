f2fd9b9e0459c1b665a0f88ee88193d39fca2f43
Michael Maton <mjmaton6@gmail.com>
2020-11-09 15:13:29 -0600

    MVP complete

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,35 @@
 import React from 'react';
+import { Link, useRouteMatch } from "react-router-dom";
 
 export default function MovieList(props) {
+  // const {url} = useRouteMatch();
+  // const { movies } = props;
+
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
+      <Link to={`/movies/${movie.id}`}>
         <MovieDetails key={movie.id} movie={movie} />
+      </Link>
       ))}
     </div>
   );
 }
+
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
 
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
+
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
     </div>
   );
 }
